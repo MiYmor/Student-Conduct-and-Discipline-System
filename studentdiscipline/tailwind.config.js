ae8683a6f2b7b3ce7b17965357400c594d43b23d
MiYmor <david.ilustre@gmail.com>
2023-09-09 20:06:52 +0800

    Initial commit

---
+++ studentdiscipline/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'templates/student/view_reports.html',
+    'templates/student/report_form.html',
+    'templates/student/home.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
